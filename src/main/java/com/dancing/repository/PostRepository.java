27d7d8cd1125a72539c055086cb0f8cf85243d21
YuanAran <19185402557@163.com>
2025-04-08 21:20:06 +0800

    Delete src directory

--- src/main/java/com/dancing/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dancing.repository;
-
-import com.dancing.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
--- src/main/java/com/dancing/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.dancing.service;
-
-import com.dancing.entity.User;
-
-public interface UserService {
-    User getUserById(Long userId);
-}
